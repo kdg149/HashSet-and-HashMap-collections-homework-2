8d9c2086ae7524a64b3eaa8473b95e853d0b85e5
Dmitriy Kyrov <kdg149@yandex.ru>
2022-10-30 16:10:31 +0300

    Main.java & WordsChecker.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
+    static final String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna" +
+            "aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint" +
+            "occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
     public static void main(String[] args) {
 
+        String word = "Lorem";
+
+        WordsChecker wordsChecker = new WordsChecker(text);
+
+        System.out.println("Есть ли в тексте слово " + word + " - " + (wordsChecker.hasWord(word) ? "Yes" : "No"));
+        word = "Alex";
+        System.out.println("Есть ли в тексте слово " + word + " - " + (wordsChecker.hasWord(word) ? "Yes" : "No"));
     }
 }
--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,3 +1,16 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class WordsChecker {
 
+    private final Set<String> words;
+
+    public WordsChecker(String text) {
+        this.words = new HashSet<>(List.of(text.split("\\P{IsAlphabetic}+")));
+    }
+
+    public boolean hasWord(String word) {
+        return words.contains(word);
+    }
 }
